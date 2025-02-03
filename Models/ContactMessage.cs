9f3e515fd2dd1e053831ef8a77b95cb15a6e1d8b
KarlaBC <karlatataianabc@gmail.com>
2025-02-02 21:46:17 -0600

    se crea modelo y conexión con la base de datos

---
+++ Models/ContactMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppFormMVC.Models
+{
+    public class ContactMessage
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de correo no válido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es obligatorio")]
+        [StringLength(500, ErrorMessage = "El mensaje no puede superar los 500 caracteres")]
+        public string Mensaje { get; set; }
+    }
+}
